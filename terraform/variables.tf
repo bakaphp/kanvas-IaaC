e8f91b220f76d90e99c3d53d53379df2e379f12b
Rafael White <rwhite@mctekk.com>
2023-01-05 11:55:14 -0400

    read variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,17 @@
-# variable "eks_cluster_name" {
-#   type        = string
-#   description = "The id of the machine image (AMI) to use for the server."
-#   default = "kanvas-cluster"
-# }
+variable "eks_cluster_name" {
+  type        = string
+  description = "The id of the machine image (AMI) to use for the server."
+  default = "kanvas-cluster"
+}
 
-# variable "eks_cluster_ec2_instance_type" {
-#   type        = string
-#   description = "The machine width for the worker node."
-#   default = "m5.large"
-# }
+variable "eks_cluster_ec2_instance_type" {
+  type        = string
+  description = "The machine width for the worker node."
+  default = "m5.large"
+}
 
-# variable "aws_region" {
-#   type        = string
-#   description = "The aws target region."
-#   default = "us-east-1"
-# }
+variable "aws_region" {
+  type        = string
+  description = "The aws target region."
+  default = "us-east-1"
+}
