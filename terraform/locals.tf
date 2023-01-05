962cfe6728c587445bdf6673edfcbba2e65dc1a5
Rafael White <rwhite@mctekk.com>
2023-01-05 11:13:52 -0400

    add environment specific variables on deploy and destroy infra. Add variable when apply

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,9 +1,5 @@
 locals {
-  _k8s_service_account_name       = "aws-load-balancer-controller"
-  _k8s_service_account_autoscaler = "cluster-autoscaler"
-  _aws_eks_iam_group              = "cluster_eks_group"
-  
-  _aws_env_s3_bucket_prefix_key    = "${terraform.workspace}"
+  _aws_env_s3_bucket_prefix_key = "${terraform.workspace}"
   vpc_name = "kanvas-iaac-vpc"
   aws_region = "us-east-1"
 }
