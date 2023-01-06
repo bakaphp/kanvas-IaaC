65e0fd9485bae5e566479b279cf2af894e0094c3
Rafael White <rwhite@mctekk.com>
2023-01-06 15:25:36 -0400

    fix issue with vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,35 +1,35 @@
-# variable "eks_cluster_name" {
-#   type        = string
-#   description = "The cluster name"
-#   default = "kanvas-cluster"
-# }
+variable "eks_cluster_name" {
+  type        = string
+  description = "The cluster name"
+  default = "kanvas-cluster"
+}
 
-# variable "eks_cluster_ec2_instance_type" {
-#   type        = string
-#   description = "The EC2 instance type"
-#   default = "m5.large"
-# }
+variable "eks_cluster_ec2_instance_type" {
+  type        = string
+  description = "The EC2 instance type"
+  default = "m5.large"
+}
 
-# variable "aws_region" {
-#   type        = string
-#   description = "The aws target region."
-#   default = "us-east-1"
-# }
+variable "aws_region" {
+  type        = string
+  description = "The aws target region."
+  default = "us-east-1"
+}
 
-# variable "vpc_cidr_block" {
-#   type        = string
-#   description = "The aws target region."
-#   default = "10.0.0.0/16"
-# }
+variable "vpc_cidr_block" {
+  type        = string
+  description = "The aws target region."
+  default = "10.0.0.0/16"
+}
 
-# variable "vpc_id" {
-#   type        = string
-#   description = "The VPC id"
-#   default = "vpc-04cfab9da09e8ededs"
-# }
+variable "vpc_id" {
+  type        = string
+  description = "The VPC id"
+  default = "vpc-04cfab9da09e8ededs"
+}
 
-# variable "vpc_private_subnets" {
-#   type        = list
-#   description = "The VPC private subnets"
-#   default = ["subnet-ac6019de","subnet-ac6019ss"]
-# }
+variable "vpc_private_subnets" {
+  type        = list
+  description = "The VPC private subnets"
+  default = ["subnet-ac6019de","subnet-ac6019ss"]
+}
