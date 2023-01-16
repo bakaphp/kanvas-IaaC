88624d8d2ce850d2aed5b816e267bff3b33c08f4
Rafael White <rwhite@mctekk.com>
2023-01-16 12:28:19 -0400

    add missing sg rules

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -20,11 +20,6 @@ module "eks" {
 
   cluster_additional_security_group_ids = ["${var.eks_vpc_security_group_id}"]
 
-  # node_security_group_additional_rules = {
-
-    
-  # }
-
   eks_managed_node_groups = {
     general = {
       min_size     = 1
@@ -46,123 +41,123 @@ module "eks" {
       # security_group_use_name_prefix = false
       # security_group_description     = "Kanvas EKS Security Group"
 
-    #   security_group_rules = {
-    #     DbIn = {
-    #       description = "Database Inbound Rule"
-    #       protocol    = "tcp"
-    #       from_port   = 3306
-    #       to_port     = 3306
-    #       type        = "ingress"
-    #       cidr_blocks = "${var.vpc_cidr_block}"
-    #     }
-    #     DbOut = {
-    #       description = "Database Outbound Rule"
-    #       protocol    = "tcp"
-    #       from_port   = 3306
-    #       to_port     = 3306
-    #       type        = "egress"
-    #       cidr_blocks = "${var.vpc_cidr_block}"
-    #     }
-    #     RedisIn = {
-    #       description = "Redis Inbound Rule"
-    #       protocol    = "tcp"
-    #       from_port   = 6379
-    #       to_port     = 6379
-    #       type        = "ingress"
-    #       cidr_blocks = "${var.vpc_cidr_block}"
-    #     }
-    #     RedisOut = {
-    #       description = "Redis Outbound Rule"
-    #       protocol    = "tcp"
-    #       from_port   = 6379
-    #       to_port     = 6379
-    #       type        = "egress"
-    #       cidr_blocks = "${var.vpc_cidr_block}"
-    #     }
-    #     MailIn = {
-    #       description = "Mail Inbound Rule"
-    #       protocol    = "tcp"
-    #       from_port   = 587
-    #       to_port     = 587
-    #       type        = "ingress"
-    #       cidr_blocks = ["0.0.0.0/0"]
-    #     }
-    #     MailOut = {
-    #       description = "Mail Outbound Rule"
-    #       protocol    = "tcp"
-    #       from_port   = 587
-    #       to_port     = 587
-    #       type        = "egress"
-    #       cidr_blocks = ["0.0.0.0/0"]
-    #     }
-    #     QueueIn = {
-    #       description = "Queue Inbound Rule"
-    #       protocol    = "tcp"
-    #       from_port   = 5672
-    #       to_port     = 5672
-    #       type        = "ingress"
-    #       cidr_blocks = ["0.0.0.0/0"]
-    #     }
-    #     QueueOut = {
-    #       description = "Queue Outbound Rule"
-    #       protocol    = "tcp"
-    #       from_port   = 5672
-    #       to_port     = 5672
-    #       type        = "egress"
-    #       cidr_blocks = ["0.0.0.0/0"]
-    #     }
-    #     QueueTLSIn = {
-    #       description = "TLS Queue Inbound Rule"
-    #       protocol    = "tcp"
-    #       from_port   = 5671
-    #       to_port     = 5671
-    #       type        = "ingress"
-    #       cidr_blocks = ["0.0.0.0/0"]
-    #     }
-    #     QueueTLSOut = {
-    #       description = "TLS Queue Outbound Rule"
-    #       protocol    = "tcp"
-    #       from_port   = 5671
-    #       to_port     = 5671
-    #       type        = "egress"
-    #       cidr_blocks = ["0.0.0.0/0"]
-    #     }
-    #     MetricsIn = {
-    #       description = "Metrics Inbound Rule"
-    #       protocol    = "tcp"
-    #       from_port   = 4443
-    #       to_port     = 4443
-    #       type        = "ingress"
-    #       cidr_blocks = ["0.0.0.0/0"]
-    #     }
-    #     MetricsOut = {
-    #       description = "Metrics Outbound Rule"
-    #       protocol    = "tcp"
-    #       from_port   = 4443
-    #       to_port     = 4443
-    #       type        = "egress"
-    #       cidr_blocks = ["0.0.0.0/0"]
-    #     }
-    #     PrometheusIn = {
-    #       description = "Prometheus Inbound Rule"
-    #       protocol    = "tcp"
-    #       from_port   = 9090
-    #       to_port     = 9090
-    #       type        = "ingress"
-    #       cidr_blocks = ["0.0.0.0/0"]
-    #     }
-    #     PrometheusOut = {
-    #       description = "Prometheus Outbound Rule"
-    #       protocol    = "tcp"
-    #       from_port   = 9090
-    #       to_port     = 9090
-    #       type        = "egress"
-    #       cidr_blocks = ["0.0.0.0/0"]
-    #     }
-    #   }
-    #   security_group_tags = {
-    #     Purpose = "Let all services in"
-    #   }
+      # security_group_rules = {
+      #   DbIn = {
+      #     description = "Database Inbound Rule"
+      #     protocol    = "tcp"
+      #     from_port   = 3306
+      #     to_port     = 3306
+      #     type        = "ingress"
+      #     cidr_blocks = "${var.vpc_cidr_block}"
+      #   }
+      #   DbOut = {
+      #     description = "Database Outbound Rule"
+      #     protocol    = "tcp"
+      #     from_port   = 3306
+      #     to_port     = 3306
+      #     type        = "egress"
+      #     cidr_blocks = "${var.vpc_cidr_block}"
+      #   }
+      #   RedisIn = {
+      #     description = "Redis Inbound Rule"
+      #     protocol    = "tcp"
+      #     from_port   = 6379
+      #     to_port     = 6379
+      #     type        = "ingress"
+      #     cidr_blocks = "${var.vpc_cidr_block}"
+      #   }
+      #   RedisOut = {
+      #     description = "Redis Outbound Rule"
+      #     protocol    = "tcp"
+      #     from_port   = 6379
+      #     to_port     = 6379
+      #     type        = "egress"
+      #     cidr_blocks = "${var.vpc_cidr_block}"
+      #   }
+      #   MailIn = {
+      #     description = "Mail Inbound Rule"
+      #     protocol    = "tcp"
+      #     from_port   = 587
+      #     to_port     = 587
+      #     type        = "ingress"
+      #     cidr_blocks = ["0.0.0.0/0"]
+      #   }
+      #   MailOut = {
+      #     description = "Mail Outbound Rule"
+      #     protocol    = "tcp"
+      #     from_port   = 587
+      #     to_port     = 587
+      #     type        = "egress"
+      #     cidr_blocks = ["0.0.0.0/0"]
+      #   }
+      #   QueueIn = {
+      #     description = "Queue Inbound Rule"
+      #     protocol    = "tcp"
+      #     from_port   = 5672
+      #     to_port     = 5672
+      #     type        = "ingress"
+      #     cidr_blocks = ["0.0.0.0/0"]
+      #   }
+      #   QueueOut = {
+      #     description = "Queue Outbound Rule"
+      #     protocol    = "tcp"
+      #     from_port   = 5672
+      #     to_port     = 5672
+      #     type        = "egress"
+      #     cidr_blocks = ["0.0.0.0/0"]
+      #   }
+      #   QueueTLSIn = {
+      #     description = "TLS Queue Inbound Rule"
+      #     protocol    = "tcp"
+      #     from_port   = 5671
+      #     to_port     = 5671
+      #     type        = "ingress"
+      #     cidr_blocks = ["0.0.0.0/0"]
+      #   }
+      #   QueueTLSOut = {
+      #     description = "TLS Queue Outbound Rule"
+      #     protocol    = "tcp"
+      #     from_port   = 5671
+      #     to_port     = 5671
+      #     type        = "egress"
+      #     cidr_blocks = ["0.0.0.0/0"]
+      #   }
+      #   MetricsIn = {
+      #     description = "Metrics Inbound Rule"
+      #     protocol    = "tcp"
+      #     from_port   = 4443
+      #     to_port     = 4443
+      #     type        = "ingress"
+      #     cidr_blocks = ["0.0.0.0/0"]
+      #   }
+      #   MetricsOut = {
+      #     description = "Metrics Outbound Rule"
+      #     protocol    = "tcp"
+      #     from_port   = 4443
+      #     to_port     = 4443
+      #     type        = "egress"
+      #     cidr_blocks = ["0.0.0.0/0"]
+      #   }
+      #   PrometheusIn = {
+      #     description = "Prometheus Inbound Rule"
+      #     protocol    = "tcp"
+      #     from_port   = 9090
+      #     to_port     = 9090
+      #     type        = "ingress"
+      #     cidr_blocks = ["0.0.0.0/0"]
+      #   }
+      #   PrometheusOut = {
+      #     description = "Prometheus Outbound Rule"
+      #     protocol    = "tcp"
+      #     from_port   = 9090
+      #     to_port     = 9090
+      #     type        = "egress"
+      #     cidr_blocks = ["0.0.0.0/0"]
+      #   }
+      # }
+      # security_group_tags = {
+      #   Purpose = "Let all services in"
+      # }
     }
   }
 }
