eee0c8fd9964dc89226e05cdf931717ef3906f35
Rafael White <rwhite@mctekk.com>
2023-01-17 10:29:51 -0400

    comment traffic out

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -163,14 +163,14 @@ resource "aws_security_group_rule" "allow_all_traffic_in" {
   security_group_id = "${var.eks_vpc_security_group_id}"
 }
 
-resource "aws_security_group_rule" "allow_traffic_out" {
-  description = "Node all egress"
-  protocol    = "-1"
-  from_port   = 0
-  to_port     = 0
-  type        = "egress"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = "${var.eks_vpc_security_group_id}"
-}
+# resource "aws_security_group_rule" "allow_traffic_out" {
+#   description = "Node all egress"
+#   protocol    = "-1"
+#   from_port   = 0
+#   to_port     = 0
+#   type        = "egress"
+#   cidr_blocks = ["0.0.0.0/0"]
+#   security_group_id = "${var.eks_vpc_security_group_id}"
+# }
 
 
